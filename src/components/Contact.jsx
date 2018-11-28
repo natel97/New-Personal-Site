1f3f06f3f7ee60f7c4284525f9e6d126a515754f
= <nate.lubitz@gmail.com>
2018-11-27 18:43:51 -0600

    open in new tab, add github. Just need mobile

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,11 +3,13 @@ import './contact.css';
 import Email from '../assets/Email.svg';
 import Facebook from '../assets/Facebook.svg';
 import LinkedIn from '../assets/LinkedIn.png';
+import GitHub from '../assets/GitHub.png';
 
 export default () => (
   <div className='contact'>
-    <a href='https://facebook.com/natelubitz'><img src={Facebook} alt='Facebook' /></a>
-    <a href='https://www.linkedin.com/in/nathanial-lubitz-472b54130' alt='LinkedIn'><img src={LinkedIn} /> </a>
+    <a href='https://facebook.com/natelubitz' target="_blank" rel="noopener noreferrer"><img src={Facebook} alt='Facebook' /></a>
+    <a href='https://www.linkedin.com/in/nathanial-lubitz-472b54130' target="_blank" rel="noopener noreferrer"><img src={LinkedIn} alt='LinkedIn' /> </a>
+    <a href='https://github.com/natel97' target="_blank" rel="noopener noreferrer"><img src={GitHub} alt='GitHub'/></a>
     <a href='mailto:nate.lubitz@gmail.com'><img src={Email} alt='Email' /></a>
   </div>
 )
